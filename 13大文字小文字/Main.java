db5b418eba29f53edbeb0aeb05bc64818550ab59
rdenden <dddrrr814@gmail.com>
2021-05-10 06:18:13 +0900

    ディレクトリ名変更

--- 13大文字小文字/Main.java
+++ 13大文字小文字/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String str = "abcde";        
         System.out.println(str.toUpperCase());
+        // toLowerCase
     }
 }
