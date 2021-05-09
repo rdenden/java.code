5c1460993312730826659a8ccd407248f960bc1a
rdenden <dddrrr814@gmail.com>
2021-05-10 00:58:38 +0900

    1

--- 1/Main.java
+++ 1/Main.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 自分の得意な言語で
-        // Let's チャレンジ！！
+       
         Scanner sc = new Scanner(System.in);
+        // char型で受け取る
         char line = sc.next().charAt(0);
         String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for (int i = 0; i < alpha.length(); i++)
+        // alpha.charAt(i)は文字列i番目取得
             if (alpha.charAt(i) == line) {
                 System.out.println(i+1);
                 break;
