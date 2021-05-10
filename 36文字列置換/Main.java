b1ec076fb0356267ed651400d84d5857ac851d75
rdenden <dddrrr814@gmail.com>
2021-05-10 23:52:25 +0900

    Create Main.java

---
+++ 36文字列置換/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        // str.replace("a","b");
+        line = line.replace("at","@");
+        System.out.println(line);
+    }
+}
