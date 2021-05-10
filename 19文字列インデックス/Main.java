22a8ad999843c74bf5930c3772f0f68357662d27
rdenden <dddrrr814@gmail.com>
2021-05-10 18:26:37 +0900

    Create Main.java

---
+++ 19文字列インデックス/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int num = sc.nextInt();
+        //String.charAt()
+        System.out.println(str.charAt(num-1));
+        //逆の操作
+        // System.out.println(str.indexOf("i"));
+    }
+}
