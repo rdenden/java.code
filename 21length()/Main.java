2d3aa8db9878a585b9d520526b959107cc36dacc
rdenden <dddrrr814@gmail.com>
2021-05-10 18:56:30 +0900

    Create Main.java

---
+++ 21length()/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line1 = sc.nextLine();
+        String line2 = sc.nextLine();
+        if (line1.length() == line2.length()) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
