4ab1f494fe816369cb021952e72ee35bcaf0317c
rdenden <dddrrr814@gmail.com>
2021-05-13 02:08:09 +0900

    69

---
+++ 69四捨五入/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        for (int i = 0;i < 7;i++) {
+            double num = sc.nextInt();
+            sum += num;
+        }
+        // 小数第一位まで出力　10倍して小数点の位置をずらす
+        double result = ((double)Math.round(sum / 7 * 10))/10;
+        System.out.println(result);
+    }
+}
