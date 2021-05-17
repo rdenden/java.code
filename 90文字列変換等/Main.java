1ed6d0176516eb0d95810c44e280128f87175ed9
rdenden <dddrrr814@gmail.com>
2021-05-17 14:21:37 +0900

    90

---
+++ 90文字列変換等/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5;i++) {
+            sum += sc.nextInt();
+        }
+        // 文字列→数値　int i = Integer.parseInt(s);
+        // 数値→文字列　String s = String.valueOf(i);
+        String s = String.valueOf(sum);
+        char result = s.charAt(s.length()-1);
+        System.out.println(result);
+    }
+}
