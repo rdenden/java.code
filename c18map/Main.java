7f0381199a18a83959e196b5b4feb59997eb1c50
rdenden <dddrrr814@gmail.com>
2021-05-22 08:20:21 +0900

    Create Main.java

---
+++ c18map/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int needNum = sc.nextInt();
+        Map<String,Integer> recipe = new HashMap<>();
+        Map<String,Integer> have = new HashMap<>();
+        for (int i = 0;i < needNum;i++) {
+            String rKey = sc.next();
+            int rKeyNum = sc.nextInt();
+            recipe.put(rKey,rKeyNum);
+        }
+        int haveNum = sc.nextInt();
+        for (int i = 0;i < haveNum;i++) {
+            String hKey = sc.next();
+            int hKeyNum = sc.nextInt();
+            have.put(hKey,hKeyNum);
+        }
+        int result = 10000;
+        boolean zero = true;
+        for (String key : recipe.keySet()) {
+            int value = recipe.get(key);
+            if (have.containsKey(key)) {
+                if (result > (have.get(key) / value)) {
+                    result = have.get(key) / value;
+                    zero = false;
+                }
+            } else {
+                zero = true;
+                break;
+            }
+        }
+        
+        
+        if (zero == false) {
+            System.out.println(result);
+        } else {
+            System.out.println(0);
+        }
+        
+    }
+}
