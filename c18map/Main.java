22438cbb7b9248f39ae0f69f9cc931aea6d9f2e2
rdenden <dddrrr814@gmail.com>
2021-05-22 08:27:59 +0900

    Update Main.java

--- c18map/Main.java
+++ c18map/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int needNum = sc.nextInt();
+        // マップを二つ作る
         Map<String,Integer> recipe = new HashMap<>();
         Map<String,Integer> have = new HashMap<>();
         for (int i = 0;i < needNum;i++) {
@@ -18,8 +19,10 @@ public class Main {
             int hKeyNum = sc.nextInt();
             have.put(hKey,hKeyNum);
         }
+        
         int result = 10000;
         boolean zero = true;
+        // マップに格納された情報を１つずつ取り出す
         for (String key : recipe.keySet()) {
             int value = recipe.get(key);
             if (have.containsKey(key)) {
