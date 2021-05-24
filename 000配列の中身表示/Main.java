d024e995f9cbad6318b73e10e594bbca907fbd64
rdenden <dddrrr814@gmail.com>
2021-05-24 16:45:23 +0900

    Update Main.java

--- 000配列の中身表示/Main.java
+++ 000配列の中身表示/Main.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        int [] array = {1,2,3,4,5,6};
-        
+        int [] array = {1,2,3,4,5,6};        
         System.out.println(Arrays.toString(array));
+        // 多次元配列の場合
+        int [][] countrys = new int [3][5];
+        System.out.println(Arrays.deepToString(countrys));
     }
 }
